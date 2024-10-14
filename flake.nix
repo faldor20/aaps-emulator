b15b587024ee3dc376b115a4711392d21b8db562
Eli Dowling <eli.jambu@gmail.com>
2024-10-14 16:07:58 +1000

    switch to pnpm

--- flake.nix
+++ flake.nix
@@ -6,6 +6,6 @@
   outputs = { flakelight, ... }@inputs:
     flakelight ./. {
       inherit inputs;
-      devShell.packages = pkgs: [ pkgs.deno   ];
+      devShell.packages = pkgs: [ pkgs.pnpm pkgs.nodejs-slim_22   ];
     };
 }
