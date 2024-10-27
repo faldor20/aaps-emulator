77fad55468289c2dddc5846f9d2fe42932f03727
Eli Dowling <eli.jambu@gmail.com>
2024-10-27 18:59:21 +1000

    set step to 0.01

--- src/lib/dataEditor.svelte
+++ src/lib/dataEditor.svelte
@@ -29,27 +29,27 @@
         profileState= overrideProfileInit(profile);
     }} />
 <!--TODO: make a way for this to be undefined so that it won't override the profiel and we don't have to stop emulatino when the orgiinal value in the profile changes-->
-<Slider label="BG Accel ISF Weight" min={0} max={1} bind:value={profileState.bgAccel_ISF_weight} />
-<Slider label="PP ISF Weight" min={0} max={1} bind:value={profileState.pp_ISF_weight} />
+<Slider label="BG Accel ISF Weight" min={0} max={1} step={0.01} bind:value={profileState.bgAccel_ISF_weight} />
+<Slider label="PP ISF Weight" min={0} max={1} step={0.01} bind:value={profileState.pp_ISF_weight} />
 
-<Slider label="Lower ISF Range Weight" min={0} max={2} step={0.1} bind:value={profileState.lower_ISFrange_weight} />
-<Slider label="Higher ISF Range Weight" min={0} max={2} step={0.1} bind:value={profileState.higher_ISFrange_weight} />
+<Slider label="Lower ISF Range Weight" min={0} max={2} step={0.01} bind:value={profileState.lower_ISFrange_weight} />
+<Slider label="Higher ISF Range Weight" min={0} max={2} step={0.01} bind:value={profileState.higher_ISFrange_weight} />
 
-<Slider label="Dura isf weight" min={0} max={5} bind:value={profileState.dura_ISF_weight} />
+<Slider label="Dura isf weight" min={0} max={5} step={0.01} bind:value={profileState.dura_ISF_weight} />
 <Folder title="SMB Settings">
-<Slider label="SMB Delivery Ratio" min={0.1} max={1.0} step={0.1} bind:value={profileState.smb_delivery_ratio} />
-<Slider label="SMB Delivery Ratio Min" min={0.1} max={1.0} step={0.1} bind:value={profileState.smb_delivery_ratio_min} />
-<Slider label="SMB Delivery Ratio Max" min={0.5} max={1.0} step={0.1} bind:value={profileState.smb_delivery_ratio_max} />
+<Slider label="SMB Delivery Ratio" min={0.01} max={1.0} step={0.01} bind:value={profileState.smb_delivery_ratio} />
+<Slider label="SMB Delivery Ratio Min" min={0.01} max={1.0} step={0.01} bind:value={profileState.smb_delivery_ratio_min} />
+<Slider label="SMB Delivery Ratio Max" min={0.5} max={1.0} step={0.01} bind:value={profileState.smb_delivery_ratio_max} />
 <Slider label="SMB Delivery Rati`o BG Range" min={0.0} max={100} step={1} bind:value={profileState.smb_delivery_ratio_bg_range} />
-<Slider label="SMB Max Range Extension" min={1.0} max={5.0} step={0.1} bind:value={profileState.smb_max_range_extension} />
+<Slider label="SMB Max Range Extension" min={1.0} max={5.0} step={0.01} bind:value={profileState.smb_max_range_extension} />
 </Folder >
-<Slider label="Auto ISF max" min={0} max={5} bind:value={profileState.autoISF_max} />
-<Slider label="Auto ISF min" min={0} max={1} bind:value={profileState.autoISF_min} />
+<Slider label="Auto ISF max" min={0} max={5} step={0.01} bind:value={profileState.autoISF_max} />
+<Slider label="Auto ISF min" min={0} max={1} step={0.01} bind:value={profileState.autoISF_min} />
 <Slider 
     label="IOB Threshold Percent" 
     min={10} 
     max={100} 
-    step={1} 
+    step={0.1} 
     bind:value={profileState.iob_threshold_percent} 
 />
 <Folder title="Non Autoisf">
